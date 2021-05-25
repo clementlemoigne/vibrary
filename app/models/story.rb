864dcb2035096a7a7a5fb3fbbb47d51e1d021a9c
Dorian Guitel <dorian.guitel@gmail.com>
2021-05-25 12:40:19 +0200

    added validations to models

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,8 +1,18 @@
 class Story < ApplicationRecord
+  TAGS = ["BDSM", "Public", "LGBTQ+", "Threesome", "Horror", "Romance", "Leather", "Feet", "Furry", "Fantasy", "Accounting"]
   belongs_to :author, class_name: "User"
   has_many :readings
   has_many :favorites
   has_many :reactions
 
   validates :title, :content, presence: true
+  validate :tags_should_be_in_the_list
+
+  private
+
+  def tags_should_be_in_the_list
+    if (TAGS & tags).size != tags.size
+      errors.add(:tags, :inclusion)
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Story.destroy_all
+
+#Bree is the reader, others are authors
+bree = User.create!(age: 48, username: "BreeVDK", email: "bree.dh@gmail.com", password: "12345", whitelist: ["Public", "Romance", "Fantasy"], blacklist: ["Furry", "Accounting", "Leather"] )
+granny = User.create!(age: 74, username: "GrannySmith", email: "aglae.sydonie@gmail.com", password: "12345", whitelist: ["Furry", "Romance", "BDSM"], blacklist: ["Public", "Horror", "Threesome"] )
+brigitte = User.create!(age: 68, username: "BrigitteM", email: "brigitte.m@elysee.fr", password: "12345", whitelist: ["LGBTQ+", "Leather", "Accounting"], blacklist: ["Threesome", "Feet", "Furry"] )
+nathalie = User.create!(age: 54, username: "BatNath", email: "nath.richon@gmail.com", password: "12345", whitelist: [ "Romance", "Fantasy"], blacklist: ["Furry", "Accounting", "Leather"] )
+
+story_granny_1 = Story.create!(
+  author: granny, 
+  tags: ["Public", "Fantasy"],
+  vibration_compatibility: true,
+  intensity: 4,
+  reading_time: 15,
+  title: "Aventure à la campagne", 
+  content: "Tout le monde la nommait la Grande-Forêt, et ce depuis la nuit des temps. 
+  Tout le monde connaissait la Grande-Forêt sans jamais y avoir mis les pieds. La Grande-Forêt, comme son nom ne l’indiquait pas, était très, très, très grande. Elle couvrait presque la moitié du continent.
+  Si sur ses lisières le peuplement semblait relativement clairsemé, il n’en allait pas de même sitôt parcourus quelques arpents."
+)
+story_granny_2 = Story.create!(
+  author: granny, 
+  tags: ["Public", "Fantasy"],
+  vibration_compatibility: true,
+  intensity: 4,
+  reading_time: 15,
+  title: "Aventure à la campagne", 
+  content: "Tout le monde la nommait la Grande-Forêt, et ce depuis la nuit des temps. 
+  Tout le monde connaissait la Grande-Forêt sans jamais y avoir mis les pieds. La Grande-Forêt, comme son nom ne l’indiquait pas, était très, très, très grande. Elle couvrait presque la moitié du continent.
+  Si sur ses lisières le peuplement semblait relativement clairsemé, il n’en allait pas de même sitôt parcourus quelques arpents."
+)
+story_granny_3
+story_brigitte_1
+story_brigitte_2
+story_brigitte_3
+story_nath_1
+story_nath_2
+story_nath_3
