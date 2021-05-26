b65a0c0aa5a6d9d7f127b59c92386f0b075d2476
Manon Schnetzler <bonsoir@mailo.com>
2021-05-26 23:02:21 +0200

    add continue readingq to home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,15 @@
 class PagesController < ApplicationController
-  
+
   skip_before_action :authenticate_user!, only: [ :landing, :buttplug ]
 
   def home
     @all_stories = Story.all
     @stories_bookmarked = Favorite.where(user_id: current_user.id).map(&:story)
+    @stories_current_reading = Reading.where(user_id: current_user.id).map(&:story)
+    # @stories_for_you = Story.where( (current_user.whitelist & Story.first.tags))
+    # @stories_for_you = Story.where(tags: current_user.whitelist)
+    # @stories_for_you = Story.includes(tags: current_user.whitelist & Story.first.tags)
+    # raise
   end
 
   def landing
