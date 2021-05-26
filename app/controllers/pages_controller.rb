13553835c0b7889b4f7810ea225ede5c6b358198
Manon Schnetzler <bonsoir@mailo.com>
2021-05-26 13:19:31 +0200

    wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:landing]
 
   def home
-    @all_stories = Story.all.reverse
+    @all_stories = Story.all
+    @stories_bookmarked = Favorite.where(user_id: current_user.id).map(&:story)
+    raise
   end
 
   def landing
+  end
+
+  private
+
+  def isFavorite?
+    @story = Story.find(params[:id])
   end
 end
