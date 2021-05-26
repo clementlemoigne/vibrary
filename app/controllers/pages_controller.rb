a70552289cdc58454e8d29955952654333d91028
Agathe Auriol <agathe.auriol@gmail.com>
2021-05-26 10:54:36 +0200

    route ajoutée, manque css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :landing ]
+  skip_before_action :authenticate_user!, only: [:landing]
 
   def home
     @stories = Story.all
