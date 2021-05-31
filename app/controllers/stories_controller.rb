13a12a9d5a114c12bb78ee8b51d6c44ec16bddd2
Agathe Auriol <agathe.auriol@gmail.com>
2021-05-31 14:28:01 +0200

    prise en charge des tags pour la search

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,7 +3,8 @@ class StoriesController < ApplicationController
     @recommended_stories = policy_scope(Story).select { |story| story.tags.any?{ |tag| current_user.whitelist.include?(tag)}}
     @recommended_stories = @recommended_stories.sort_by { |s| s.score_for_user(current_user) }.reverse
     @best_stories = policy_scope(Story).sort_by { |story| story.score }.reverse
-    @searched_stories = Story.global_search(params[:query])
+    searched_terms = [params[:query], params[:tag]].flatten.reject(&:blank?)
+    @searched_stories = Story.global_search(searched_terms)
     @searched_stories = policy_scope(@searched_stories)
   end
 
