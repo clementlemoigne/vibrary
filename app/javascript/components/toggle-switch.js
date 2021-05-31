72413c58a23e99804a0d7da15cf7d7c15d19129d
clement le moigne <clement.lemoigne@gmail.com>
2021-05-31 12:43:22 +0200

    bookmark on home page & show story page

--- app/javascript/components/toggle-switch.js
+++ app/javascript/components/toggle-switch.js
@@ -1,15 +1,17 @@
 const toggleVibration = () => {
-  const x = document.querySelector(".vibrating-zone");
-  const btn = document.querySelector("#toggle-scan");
-  console.log(x)
-  btn.addEventListener("click", (event) => {
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  if (document.querySelector("#toggle-scan")) {
+    const x = document.querySelector(".vibrating-zone");
+    const btn = document.querySelector("#toggle-scan");
+    console.log(x)
+    btn.addEventListener("click", (event) => {
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
 
-  });
-}; 
+    });
+  }
+};
 
 export { toggleVibration }
